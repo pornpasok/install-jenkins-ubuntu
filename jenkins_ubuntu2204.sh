0e112afd939d05779971b1edd0b7d2039d6970cd
PORNPASOK SOOKYEN <ton350d@gmail.com>
2023-04-03 10:00:31 +0700

    Update jenkins_ubuntu2204.sh

--- jenkins_ubuntu2204.sh
+++ jenkins_ubuntu2204.sh
@@ -1,8 +1,11 @@
 sudo apt install default-jdk -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
 
